aca8f99d41bbc8dcf0d3647dfc36b4ba54406647
kako57 <71569154+kako57@users.noreply.github.com>
2022-07-09 18:32:22 -0400

    use strtol instead of atoi

--- main.c
+++ main.c
@@ -1,25 +1,24 @@
+#include <string.h>
+
 #include "fraction.h"
 #include "fraction_list.h"
 #include "polynomial.h"
 #include "solution.h"
 #include "int_list.h"
-#include <string.h>
 
 int main(int argc, char **argv) {
-  if (argc < 2) {
+  if (argc < 3) {
     fprintf(stderr, "Usage: %s [coefficients in decreasing power]\n", argv[0]);
     return 1;
   }
 
-  polynomial p = {.degree = argc - 2};
-
-  p.coefficients = (fraction *)malloc((p.degree + 1) * sizeof(fraction));
+  polynomial p = {(fraction *)malloc((argc - 1) * sizeof(fraction)), argc - 2};
 
   for (int i = p.degree; i >= 0; i--) {
     char *num = strtok(argv[p.degree - i + 1], "/");
     char *den = strtok(NULL, "/");
 
-    p.coefficients[i] = (fraction) { atoi(num), den == NULL ? 1 : atoi(den) };
+    p.coefficients[i] = (fraction){ strtol(num, NULL, 10), den == NULL ? 1 : strtol(den, NULL, 10) };
   }
 
   solution_node *results = solve(&p);
